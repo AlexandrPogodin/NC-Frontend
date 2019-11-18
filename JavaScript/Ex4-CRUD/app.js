6a513a4fdacfec7b1d9cc6c778a5b8a8922cff03
Alexandr Pogodin <awpogodin@mail.ru>
2019-11-18 19:27:18 +0400

    Update

--- JavaScript/Ex4-CRUD/app.js
+++ JavaScript/Ex4-CRUD/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable no-constant-condition */
 /* eslint-disable no-use-before-define */
 const readline = require('readline');
@@ -28,7 +29,16 @@ const question = str => new Promise(resolve => rl.question(str, resolve));
 const operations = {
   readFile: async () => {
     const dirPath = path.resolve(__dirname, 'db');
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
     const filePath = path.resolve(dirPath, 'db.json');
+    if (!fs.existsSync(filePath)) {
+      const file = JSON.stringify([]);
+      fs.writeFile(filePath, file, err => {
+        if (err) throw err;
+      });
+    }
     try {
       const file = fs.readFileSync(filePath, 'utf8', err => {
         if (err) throw err;
@@ -49,7 +59,6 @@ const operations = {
       const file = JSON.stringify(data);
       fs.writeFile(filePath, file, err => {
         if (err) throw err;
-        console.log('File saved.');
       });
     } catch (err) {
       throw err;
@@ -57,12 +66,56 @@ const operations = {
   },
   inputContent: async () => {
     console.log('Enter fields, please');
-    const name = await question('Name: ');
-    const developers = await question('Developers: ');
-    const platforms = await question('Platforms: ');
-    const genres = await question('Genres: ');
+    let isNameValid = false;
+    let isDevelopersValid = false;
+    let isPlatformsValid = false;
+    let isGenresValid = false;
+
+    let name;
+    let developers;
+    let platforms;
+    let genres;
+    while (!isNameValid) {
+      name = await question('Name: ');
+      const data = await operations.readFile();
+      const indexOfGame = data.findIndex(item => item.name === name);
+      if (!name) {
+        console.error('ERROR: You entered an empty string!');
+      } else if (indexOfGame !== -1) {
+        console.error(`ERROR: Game "${name}" already exists!`);
+      } else {
+        isNameValid = true;
+      }
+    }
+
+    while (!isDevelopersValid) {
+      developers = await question('Developers: ');
+      if (!developers) {
+        console.error('ERROR: You entered an empty string!');
+      } else {
+        isDevelopersValid = true;
+      }
+    }
+
+    while (!isPlatformsValid) {
+      platforms = await question('Platforms: ');
+      if (!platforms) {
+        console.error('ERROR: You entered an empty string!');
+      } else {
+        isPlatformsValid = true;
+      }
+    }
+
+    while (!isGenresValid) {
+      genres = await question('Genres: ');
+      if (!genres) {
+        console.error('ERROR: You entered an empty string!');
+      } else {
+        isGenresValid = true;
+      }
+    }
     const content = new Game(name, developers, platforms, genres);
-    console.log(content);
+    console.log('Added game: ', content);
     return content;
   },
   start: async () => {
@@ -74,42 +127,44 @@ const operations = {
 0 - Exit
     `);
     const answer = await question('Choose an operation (0-4): ');
-    if (+answer === 1) return operations.create();
-    if (+answer === 2) return operations.read();
-    if (+answer === 3) return operations.update();
-    if (+answer === 4) return operations.delete();
+    answer.toLocaleLowerCase();
+    if (+answer === 1 || answer.startsWith('c')) return operations.create();
+    if (+answer === 2 || answer.startsWith('r')) return operations.read();
+    if (+answer === 3 || answer.startsWith('u')) return operations.update();
+    if (+answer === 4 || answer.startsWith('d')) return operations.delete();
 
-    if (+answer === 0) return operations.end();
-    console.error('Expected number 0-4');
-    operations.start();
+    if (answer === 0) return operations.end();
+    console.error('ERROR: Expected number 0-4');
+    return operations.start();
   },
   create: async () => {
-    console.log('\n CREATE');
+    console.log('\n--- CREATE ---');
     const data = await operations.readFile();
-    while (true) {
+    let isUserWantsMore = true;
+    while (isUserWantsMore) {
       const content = await operations.inputContent();
       data.push(content);
       const answer = await question('Add another game? (enter "y"): ');
-      if (!(answer.startsWith('Y') || answer.startsWith('y'))) break;
+      if (!(answer.startsWith('Y') || answer.startsWith('y'))) {
+        isUserWantsMore = false;
+      }
     }
     await operations.saveFile(data);
-    operations.start();
+    console.log('File saved.');
+    return operations.start();
   },
   read: async () => {
-    console.log('\n READ');
+    console.log('\n--- READ ---');
     const data = await operations.readFile();
     console.log(`Number of records: ${data.length}`);
     console.log(data);
-    operations.start();
+    return operations.start();
   },
   update: async () => {
-    console.log('\n UPDATE');
+    console.log('\n--- UPDATE ---');
     const data = await operations.readFile();
     const name = await question('Enter name to search: ');
-    let indexOfGame;
-    await data.forEach((item, i) => {
-      if (item.name === name) indexOfGame = i;
-    });
+    const indexOfGame = data.findIndex(item => item.name === name);
     if (data[indexOfGame]) {
       console.log('Game found: ', data[indexOfGame]);
       console.log('Editing...');
@@ -119,22 +174,19 @@ const operations = {
       copyData.push(content);
       copyData.push(...data.slice(indexOfGame + 1));
       await operations.saveFile(copyData);
-      operations.start();
-    } else {
-      console.log('Game not found.');
-      return operations.update();
+      console.log('File updated.');
+      return operations.start();
     }
+    console.log('Game not found.');
+    return operations.update();
   },
   delete: async () => {
-    console.log('\n DELETE');
+    console.log('\n--- DELETE ---');
     const data = await operations.readFile();
     const name = await question('Enter name to search: ');
-    let indexOfGame;
-    await data.forEach((item, i) => {
-      if (item.name === name) indexOfGame = i;
-    });
+    const indexOfGame = data.findIndex(item => item.name === name);
     if (data[indexOfGame]) {
-      console.log('Game found.');
+      console.log('Game found: ', data[indexOfGame]);
       const answer = await question(
         `Are you sure you want to delete ${data[indexOfGame].name}? (enter "y")`
       );
@@ -143,12 +195,12 @@ const operations = {
         copyData.push(...data.slice(0, indexOfGame));
         copyData.push(...data.slice(indexOfGame + 1));
         await operations.saveFile(copyData);
+        console.log('Game successfully deleted.');
       }
-      operations.start();
-    } else {
-      console.log('Game not found.');
-      return operations.update();
+      return operations.start();
     }
+    console.log('Game not found.');
+    return operations.delete();
   },
   end: async () => {
     rl.close();
